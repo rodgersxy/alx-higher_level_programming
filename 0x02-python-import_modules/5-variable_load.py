eaa4b472a660dc1d5147aa44d80994612ddeb354
rodgersxy <rodgersny99@gmail.com>
2023-05-11 08:21:32 -0700

    5-variable_load.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""program that imports the variable a from the file variable_load_5.py and prints"""
+
+if __name__ == "__main__":
+
+    from variable_load_5 import a
+
+print("{}".format(a))
