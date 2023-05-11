7f6cda746bde855957e28493a869a708157dae2b
rodgersxy <rodgersny99@gmail.com>
2023-05-11 08:24:18 -0700

    pycodestyle fix

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""program that imports the variable a from the file variable_load_5.py and prints"""
+"""program that imports the variable a from variable_load_5.py"""
 
 if __name__ == "__main__":
 
