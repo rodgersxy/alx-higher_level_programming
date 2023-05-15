b9f448ac1526acc8e2fccbc4c5306f199d52e0c1
rodgersxy <rodgersny99@gmail.com>
2023-05-14 23:29:53 -0700

    adds two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Function that adds two tuples
+# tuple_a: first tuple
+# tuple_b: second tuple
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a = (0, 0)
+    if len(tuple_b) < 2:
+        tuple_b = (0, 0)
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
