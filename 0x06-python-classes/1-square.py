2b2144381e5b01447ec87b059caa24ef11fffcca
rodgersxy <rodgersny99@gmail.com>
2023-05-30 08:59:22 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class square that defines square"""
+
+
+class Square:
+    """class Square has a private instance"""
+    def __init__(self, size):
+
+        """
+        __size: made private by prefix
+        __init__: instatiation andinitialization
+        """
+        self.__size = size
