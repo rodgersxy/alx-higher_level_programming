59c0d64bbf00bdd02b163f54ba8a9ee96da3b6d4
rodgersxy <rodgersny99@gmail.com>
2023-06-13 13:41:13 -0700

    File: 11-student.py bug fix

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""a class Student that defines a student"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Student class has three public instance attributes:
+        first_name, last_name, and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        to_json method to retrieve dict
+        """
+        if type(attrs) is list:
+            return {x: self.__dict__.get(x)
+                    for x in attrs if x in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        to replace attributes of Student instance
+        json - dictionary
+        """
+        for x, y in json.items():
+            self.__dict__[x] = y
