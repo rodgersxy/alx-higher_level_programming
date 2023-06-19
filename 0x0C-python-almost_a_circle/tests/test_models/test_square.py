0625b83117245b1b79a289b4afc7ccffa410b0cd
rodgersxy <rodgersny99@gmail.com>
2023-06-19 04:42:50 -0700

    square test case

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -90,6 +90,88 @@ class TestSquare(unittest.TestCase):
         with self.assertRaises(TypeError):
             Square(float('nan'), 10, 8)
 
+    def test_boolean_size_type_error(self):
+        """
+        square with a boolean
+        """
+        with self.assertRaises(TypeError):
+            Square(True, 9, 1)
+
+    def test_boolean_x_type_error(self):
+        with self.assertRaises(TypeError):
+            Square(2, True)
+
+    def test_boolean_y_type_error(self):
+        with self.assertRaises(TypeError):
+            Square(2, 1, True)
+
+    def test_none_size_type_error(self):
+        """
+        square with None
+        """
+        with self.assertRaises(TypeError):
+            Square(None)
+
+    def test_none_x_type_error(self):
+        with self.assertRaises(TypeError):
+            Square(9, None)
+
+    def test_none_y_type_error(self):
+        with self.assertRaises(TypeError):
+            Square(2, 9, None)
+
+    # test case for __str__
+
+    def test_str_representation(self):
+        """
+        square and check its string representation
+        """
+        square = Square(2, 3, 4, 5)
+        expected_str = "[Square] (5) 3/4 - 2"
+        self.assertEqual(str(square), expected_str)
+
+    # test case for to_dictionary
+
+    def test_to_dictionary(self):
+        """
+        to a dictionary
+        """
+        square = Square(2, 3, 4, 5)
+        expected_dict = {'id': 5, 'size': 2, 'x': 3, 'y': 4}
+        self.assertEqual(square.to_dictionary(), expected_dict)
+
+    # test case for _update *args and **kwargs
+
+    def test_update(self):
+        """
+        update using args then with kwargs
+        """
+        square = Square(2, 3, 4, 5)
+        # square using *args
+        square.update(6, 7, 8, 9)
+        self.assertEqual(square.id, 6)
+        self.assertEqual(square.size, 7)
+        self.assertEqual(square.x, 8)
+        self.assertEqual(square.y, 9)
+
+        # **kwargs
+        square.update(id=10, size=11, x=12, y=13)
+        self.assertEqual(square.id, 10)
+        self.assertEqual(square.size, 11)
+        self.assertEqual(square.x, 12)
+        self.assertEqual(square.y, 13)
+
+    # test case Area
+
+    def test_area(self):
+        """
+        Calculate the area
+        """
+        square = Square(5)
+        area = square.area()
+
+        self.assertEqual(area, 25)
+
 
 if __name__ == '__main__':
     unittest.main()
