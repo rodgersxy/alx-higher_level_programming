8bc020fb848a1acf75ee19f362536a5163a6e454
rodgersxy <rodgersny99@gmail.com>
2023-07-18 12:45:58 -0700

    bug fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,3 +2,7 @@
 -- (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table
+CHANGE name name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
