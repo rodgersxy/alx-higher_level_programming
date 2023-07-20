c229af43e64a7edf2138b3eb8f99735cf6752f95
rodgersxy <rodgersny99@gmail.com>
2023-07-19 22:03:03 -0700

    error fix

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 -- Each record should display: tv_shows.title - rating sum
 -- Results must be sorted in descending order by the rating
-SELECT `tv_shows`.`title`, SUM(`ratings`.`rating`) AS `rating_sum`
+SELECT `tv_shows`.`title`, SUM(ratings.rating) AS `rating_sum`
 FROM `tv_shows`
 JOIN `ratings` ON `tv_shows`.`id` = `ratings`.`tv_show_id`
 GROUP BY `tv_shows`.`title`
