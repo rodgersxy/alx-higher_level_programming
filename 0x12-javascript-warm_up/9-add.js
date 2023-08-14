b2820cf7a93e3653c9c11cab401c8878e8cc9b9c
rodgersxy <rodgersny99@gmail.com>
2023-08-14 12:53:45 -0700

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return (a + b);
+}
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+const results = add(a, b);
+console.log(results);
