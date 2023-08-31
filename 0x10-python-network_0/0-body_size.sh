bac74cf53dc286c1c11ebaa80859dc242065039b
rodgersxy <rodgersny99@gmail.com>
2023-08-31 04:13:50 -0700

    0-body_size.sh

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Bash script that takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response
+curl -Is "$1" | grep Content-Length | cut -f2 -d' '
