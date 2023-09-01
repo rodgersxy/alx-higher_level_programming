6690985964da532e2e16a3450eb9b0d831d6e7c1
rodgersxy <rodgersny99@gmail.com>
2023-09-01 11:53:17 -0700

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter, sends a POST request to a specified
+URL with the letter as a parameter, and handles the response
+"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) > 1:
+        q = {'q': argv[1]}
+    else:
+        q = {'q': ''}
+    res = requests.post(url, data=q)
+    try:
+        body = res.json()
+        if len(body) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(body["id"], body["name"]))
+    except:
+        print("Not a valid JSON")
