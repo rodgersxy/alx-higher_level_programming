25832b6177142e5e9209549d02801cdb1d8d8898
rodgersxy <rodgersny99@gmail.com>
2023-09-01 13:50:33 -0700

    changes

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,15 +3,15 @@
 Script to display GitHub user ID using Basic Authentication
 """
 
-import requests
+from requests import get
 from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == "__main__":
     username = argv[1]
     token = argv[2]
-    url = f"https://api.github.com/users/{username}"
-    response = requests.get(url, auth=HTTPBasicAuth(username, token))
+    url = 'https://api.github.com/user'
+    response = get(url, auth=HTTPBasicAuth(username, token))
 
     try:
         user_data = response.json()
