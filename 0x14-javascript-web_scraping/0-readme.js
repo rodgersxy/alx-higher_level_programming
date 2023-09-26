f246dcbf7883b02d9e3bb9dc8906f96401b6a980
rodgersxy <rodgersny99@gmail.com>
2023-09-26 03:30:13 -0700

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const path = process.argv[2];
+
+fs.readFile(path, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
