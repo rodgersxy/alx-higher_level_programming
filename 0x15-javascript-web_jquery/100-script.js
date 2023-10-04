a66642701ed62dbf3d06447cb4df79b5d7c03610
rodgersxy <rodgersny99@gmail.com>
2023-10-04 03:54:48 -0700

    updates

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+/* script that updates the text color of the <header> element to red (#FF0000 */
+
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.querySelector('header');
+  element.style.color = '#FF0000';
+});
