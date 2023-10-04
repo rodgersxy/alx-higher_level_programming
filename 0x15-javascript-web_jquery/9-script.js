3d82a0c773bc5a90794e2ce1222d081f726afc21
rodgersxy <rodgersny99@gmail.com>
2023-10-04 03:19:35 -0700

    9-script.js

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,6 @@
+/* fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the
+ * value of hello from that fetch in the HTML tag DIV#hello */
+
+$.get('https://fourtonfish.com/hellosalut/?lang=fr', (data) => {
+  $('DIV#hello').text(data.hello);
+});
